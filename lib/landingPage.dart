ad5c1303c86e2479af3169c12ca463fbb6dc6883
Soobinhand <chelsea9528@naver.com>
2022-07-09 20:00:02 +0900

    랜딩페이지, 메인페이지 생성

---
+++ lib/landingPage.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:dgblife_lunch/mainPage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LandingPage extends StatefulWidget{
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage>{
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3),(){
+      Get.to(MainPage());
+    });
+  }
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        color: Colors.blueAccent,
+        child: Center(child: Text("점심은 우리의 생명이니까")),
+      ),
+    );
+  }
+}
